48e74f5d3cb4759ac719fd01280d1f443a9e7377
Yao Xiao <xiaoyao227192@163.com>
2025-07-07 10:54:32 +0800

    Upload __init__.py

--- basicsr/data/__init__.py
+++ basicsr/data/__init__.py
@@ -1,6 +1,8 @@
-"""Basic data modules"""
+"""Data module for the PADUM project."""
 from basicsr.data.derain_paired_dataset import DerainPairedDataset
+from basicsr.data.paired_image_dataset import PairedImageDataset
 
 __all__ = [
-    'DerainPairedDataset'
+    'DerainPairedDataset',
+    'PairedImageDataset'
 ]
